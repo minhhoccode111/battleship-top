562f14ee2bd6bdc7d7fe76a75b22ae38fd6d0b6e
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-10-03 17:04:49 +0700

    setup some files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log('Hello, World! From index.js');
+
+import './css/nav.css';
+import './css/main.css';
+import './css/aside.css';
+import './css/reset.css';
+import './css/style.css';
+import './css/footer.css';
+import './css/header.css';
+import './css/animation.css';
+import './css/responsive.css';
+
+// import './assets/icon';
+// import './assets/img';
+
+import './js/app.js';
+import './js/animation.js';
+import './js/dom.js';
---
+++ src/test/index.test.js
@@ -0,0 +1,8 @@
+import * as index from './../index.js';
+
+xtest('specify some tests', () => {});
+xtest('specify some tests', () => {});
+xtest('specify some tests', () => {});
+xtest('specify some tests', () => {});
+xtest('specify some tests', () => {});
+xtest('specify some tests', () => {});
